b3a36c06d0b0e7dba1837b2111f0a0a8942223c7
root <root@ip-172-31-34-72.eu-west-1.compute.internal>
2015-02-16 10:43:45 +0000

    massimo 16/02

---
+++ chef-repo/cookbooks/OpenEyes/CHANGELOG.md
@@ -0,0 +1,13 @@
+OpenEyes CHANGELOG
+==================
+
+This file is used to list changes made in each version of the OpenEyes cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of OpenEyes
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ chef-repo/cookbooks/OpenEyes/recipes/default.rb
@@ -0,0 +1,72 @@
+#
+# Cookbook Name:: OpenEyes
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "apt"
+
+package 'apache2' do
+  action :install
+end
+
+service 'apache2' do
+  action [ :enable, :start ]
+end
+
+
+
+package 'libapache2-mod-php5' do
+  action :install
+end
+
+package 'php5-cli' do
+  action :install
+end
+
+package 'php5-mysql' do
+  action :install
+end
+
+package 'php5-ldap' do
+  action :install
+end
+
+##
+
+execute "git clone oe" do
+  command "cd /var/www && git clone https://github.com/openeyes/OpenEyes.git openeyes"
+end
+
+## Initialise the yii framework:
+
+execute "yii framework" do
+  command "cd /var/www/openeyes && git submodule init && git submodule update"
+end
+
+## index nad .htaccess
+execute "index and htaccess" do
+  command "cd /var/www/openeyes && mv index.example.php index.php; mv .htaccess.sample .htaccess"
+end
+
+## permissions for the assets, cache and runtime directories
+execute "permission" do
+  command "mkdir /var/www/openeyes/protected/runtime /var/www/openeyes/cache /var/www/openeyes/protected/cache && chmod 777 /var/www/openeyes/assets /var/www/openeyes/cache /var/www/openeyes/protected/cache /var/www/openeyes/protected/runtime"
+end
+
+## Cp sampl;e data
+
+execute "sample data" do
+  command "cd /var/www/openeyes && mkdir protected/config/local/ && cp protected/config/local.sample/common.sample.php protected/config/local/common.php"
+end
+
+## Enable mod_rew
+execute "mode rewrite" do
+  command "ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/" 
+end
+
+service 'apache2' do
+  action [ :restart ]
+end
