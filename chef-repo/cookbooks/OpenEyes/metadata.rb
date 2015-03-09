7362523b369abad87acc80a01e08edaee3abae85
MassimoDanieli <massimo.danieli@openeyes.org.uk>
2015-03-09 10:41:47 +0000

    Update metadata.rb

--- chef-repo/cookbooks/OpenEyes/metadata.rb
+++ chef-repo/cookbooks/OpenEyes/metadata.rb
@@ -1,6 +1,6 @@
 name             'OpenEyes'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Massimo Danieli'
+maintainer_email 'massimo.danieli@openeyes.org.uk'
 license          'All rights reserved'
 description      'Installs/Configures OpenEyes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
