b5338e9e6ce9bb578dc8524796511318078afabc
MassimoDanieli <massimo.danieli@openeyes.org.uk>
2015-03-09 11:31:04 +0000

    Update default.rb

--- chef-repo/cookbooks/OpenEyes/recipes/default.rb
+++ chef-repo/cookbooks/OpenEyes/recipes/default.rb
@@ -8,6 +8,14 @@
 #
 include_recipe "apt"
 
+mysql_service 'default' do
+  version '5.7'
+  bind_address '0.0.0.0'
+  port '3306'  
+  initial_root_password 'Ch4ng3me'
+  action [:create, :start]
+end
+
 package 'apache2' do
   action :install
 end
